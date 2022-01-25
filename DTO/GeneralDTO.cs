6210aceeadd66f1a56966925e192a1f37bd5b9bd
Somto Amaugo <Somto.amaugo@wemabank.com>
2022-01-25 15:26:05 +0100

    fixed

---
+++ DTO/GeneralDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EncryptionWebAPI.DTO
+{
+    public class DataToSendDTO
+    {
+        public string key1_enc { get; set; }
+    }
+}
--- Data/EncryptionWebRepo.cs
+++ Data/EncryptionWebRepo.cs
@@ -10,6 +10,11 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using System.DirectoryServices;
 using Microsoft.IdentityModel.Tokens;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Headers;
+using EncryptionWebAPI.DTO;
+using System.Text.Json;
 
 namespace EncryptionWebAPI.Data
 {
@@ -36,7 +41,41 @@ namespace EncryptionWebAPI.Data
         public async Task<IEnumerable<PcAccounts1B>> DecryptAllData()
         {
             var accountids = await context.PcAccounts1Bs.ToListAsync();
+            var decrypedData = await GetDecryptedData(accountids);
+            await SaveAllData(decrypedData);
             return accountids;
+        }
+
+        private async Task<List<PcAccounts1B>> GetDecryptedData(List<PcAccounts1B> data)
+        {
+            var username = "postcard_encrypt";
+            var password = "Wema@1234";
+            var url = "http://172.52.12.23:5050/cms/api/v2/crypto/encrypt-decrypt?operation=2";
+
+            using var client = new HttpClient();
+            var authToken = Encoding.ASCII.GetBytes($"{username}:{password}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+
+            foreach (var dat in data)
+            {
+
+                var dataToSend = new DataToSendDTO
+                {
+                    key1_enc = dat.AccountIdEncrypted
+                };
+                var toSend = JsonSerializer.Serialize(dataToSend);
+                var requestContent = new StringContent(toSend, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(url, requestContent);
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+                var decryptedContent = JsonSerializer.Deserialize<dynamic>(content);
+
+                dat.AccountIdEncrypted = decryptedContent.key1_enc_dec;
+            }
+
+            return data;
         }
 
         public async Task<Boolean> SaveAllData(List<PcAccounts1B> model)
@@ -60,7 +99,7 @@ namespace EncryptionWebAPI.Data
 
                 return true;
             }
-            
+
         }
     }
 }
